6a92cbef28548262aa7a3ebc8bf01edadf431c13
weijian <21yanweijian@163.com>
2017-07-05 18:12:01 +0800

    提交

--- Queue/Queue.php
+++ Queue/Queue.php
@@ -9,14 +9,42 @@ abstract class Queue {
      */
     protected $queue_config;
 
-    public function __construct($queue_alias) {
-        $queue_config = \Core\Config::get("queue/{$queue_alias}");
-        if ($queue_config && is_array($queue_config)) {
-            $this->queue_config = $queue_config;
-        } else {
-            throw new \Core\ConsumerException('队列配置不存在', 1100);
-        }
+    /**
+     * @var array 
+     */
+    static private $_cache = array();
+
+    private function __construct($queue_config) {
+        $this->queue_config = $queue_config;
     }
 
-    abstract public function pull();
+    abstract public function pop();
+
+    abstract public function close();
+
+    /**
+     * @param string $alias
+     * @return \Queue
+     */
+    static public function instance($alias) {
+        if (!static::$_cache[$alias]) {
+            $queue_config = \Core\Config::get("queue/{$alias}");
+            if ($queue_config && is_array($queue_config)) {
+                switch ($queue_config['type']) {
+                    case 'redis':
+                        static::$_cache[$alias] = new \Queue\Redis($queue_config);
+                        break;
+                    case 'kafka':
+                        static::$_cache[$alias] = new \Queue\Kafka($queue_config);
+                        break;
+                    default:
+                        throw new \Core\ConsumerException('不明队列类型', 1200);
+                }
+            } else {
+                throw new \Core\ConsumerException('队列配置不存在', 1200);
+            }
+        }
+        return static::$_cache[$alias];
+    }
+
 }
--- Queue/Redis.php
+++ Queue/Redis.php
@@ -4,7 +4,12 @@ namespace Queue;
 
 class Redis extends Queue {
 
-    public function pull() {
+    public function pop() {
+        $redis = \Core\Driver\Redis::instance($this->queue_config['source']);
+        $data = $redis->rPop($this->queue_config['key']);
+    }
+
+    public function close() {
         
     }
 
